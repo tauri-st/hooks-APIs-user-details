52cd0bccc01f172c1dad5a603003cb004de9c619
tauri_s <tauristclaire@gmail.com>
2023-12-22 15:48:33 -0800

    Add output of email in div of UserDetails

--- src/UserDetails.js
+++ src/UserDetails.js
@@ -2,7 +2,7 @@ const UserDetails = ({data}) => (
   <div className="user-details">
     <p className="name">{data.name.title} {data.name.first} {data.name.last}</p>
     <p className="country">{data.location.country}</p>
-    <p className="email"></p>
+    <p className="email">{data.email}</p>
     <figure>{/* Place image tag here */}</figure>
     <button>Fetch Another</button>
   </div>
