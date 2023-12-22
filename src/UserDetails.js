e894193e60f2f2163dba6ea8dd75eb4e35f54478
tauri_s <tauristclaire@gmail.com>
2023-12-22 15:47:05 -0800

    Add output of location.country in div of UserDetails

--- src/UserDetails.js
+++ src/UserDetails.js
@@ -1,7 +1,7 @@
 const UserDetails = ({data}) => (
   <div className="user-details">
     <p className="name">{data.name.title} {data.name.first} {data.name.last}</p>
-    <p className="country"></p>
+    <p className="country">{data.location.country}</p>
     <p className="email"></p>
     <figure>{/* Place image tag here */}</figure>
     <button>Fetch Another</button>
