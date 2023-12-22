95fa79c6f91edbe41d21bbdc43535fa89bcfaa5f
tauri_s <tauristclaire@gmail.com>
2023-12-22 15:18:52 -0800

    Add coment and console of request to check on status of request and add conditional statement to check for success status before updating data and status states

--- src/App.js
+++ src/App.js
@@ -12,8 +12,15 @@ const App = () => {
     const fetchData = async () => {
       try {
         const request = await fetch("https://randomuser.me/api/");
+        // Check out what request gives before extracting JSON 
+        // to see the status of the request, looking for
+        // request.status = 200 success status
+        console.log(request);
         const {results} = await request.json();
-        setData(results[0]);
+        if (request.status === 200) {
+          setData(results[0]);
+          setStatus("success");
+        }
         console.log(data);
         //console.log(results[0]);
       } catch (error) {
