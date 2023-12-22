eedc94f4cc339cff2d21cdc0ab2a4ac116cbeeb1
tauri_s <tauristclaire@gmail.com>
2023-12-22 13:47:00 -0800

    Add useEffect hook with async fetch with try and catch functions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ const App = () => {
   const [data, setData] = useState({});
   const [status, setStatus] = useState("loading");
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const request = await fetch("https://randomuser.me/api/");
+      } catch {
+
+      }
+    };
+  }, []);
+
   return (
     <div className="App">
       <UserDetails />
