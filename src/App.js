b9fdf46e70e8264a965cf7fbcd777ca2d81ee688
tauri_s <tauristclaire@gmail.com>
2023-12-22 16:17:13 -0800

    Move fetchData function out of useEffect hook scope

--- src/App.js
+++ src/App.js
@@ -8,28 +8,31 @@ const App = () => {
   const [data, setData] = useState({});
   const [status, setStatus] = useState("loading");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setStatus("loading");
-        const request = await fetch("https://randomuser.me/api/");
-        // Check out what request gives before extracting JSON 
-        // to see the status of the request, looking for
-        // request.status = 200 success status
-        console.log(request);
-        const {results} = await request.json();
+  // fetchData needs to be outside of the useEffect hook so that
+  // it is available to to pass to UserDetails as a prop
+  const fetchData = async () => {
+    try {
+      setStatus("loading");
+      const request = await fetch("https://randomuser.me/api/");
+      // Check out what request gives before extracting JSON 
+      // to see the status of the request, looking for
+      // request.status = 200 success status
+      console.log(request);
+      const {results} = await request.json();
 
-        if (request.status === 200) {
-          setData(results[0]);
-          setStatus("success");
-        }
-        console.log(data);
-        //console.log(results[0]);
-      } catch (error) {
-        console.log("Oh no something went wrong!", error);
-        setStatus("error");
+      if (request.status === 200) {
+        setData(results[0]);
+        setStatus("success");
       }
-    };
+      console.log(data);
+      //console.log(results[0]);
+    } catch (error) {
+      console.log("Oh no something went wrong!", error);
+      setStatus("error");
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, []);
 
